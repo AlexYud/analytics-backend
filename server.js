412c5543a601ce47480435f00f4bbb7b74b97382
Alex Yud <alexandre.yudi.oliveira@gmail.com>
2025-03-16 18:01:22 -0300

    change port to 4000

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
